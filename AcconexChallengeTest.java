012bc3e54cc61607ffbb362e051234b30b00b472
praveensg <praveen66.uvce@gmail.com>
2016-04-20 20:00:26 +0530

    Create AcconexTest

---
+++ AcconexChallengeTest.java
@@ -0,0 +1,19 @@
+package com.acconex;
+
+import org.junit.Test;
+import com.acconex.AcconexChallenge;
+import static org.assertj.core.api.Assertions.assertThat; 
+
+public class AcconexChallengeTest {
+	
+	AcconexChallenge acconexChallenge  = new AcconexChallenge();
+	double initialSum = 0;
+	
+	@Test
+	public void shouldOutputPossibleReplacementsForGivenNumber(){
+		String expected = "";
+		assertThat();		
+	}
+
+	
+}
